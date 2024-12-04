526744a8c45b1d6d5f638e0585c3990863f3e0ef
Arquake <arquake2@gmail.com>
2024-12-04 13:41:32 +0100

    PATCH
    fix avec le nouveau back-end du menu

--- frontend/src/store/userStore.ts
+++ frontend/src/store/userStore.ts
@@ -1,12 +1,11 @@
 import { create } from "zustand";
 import {combine} from "zustand/middleware";
-import {Account} from "../types/Account.ts";
 
 
 export const userStore = create(
     combine({
-        account: undefined as undefined | null | Account
+        account: null as null | true
     }, (set) => ({
-        setAccount: (account: Account | null): void => set({account})
+        setAccount: (account: true | null): void => set({account})
     }))
 );
--- frontend/src/useHook/useAuth.ts
+++ frontend/src/useHook/useAuth.ts
@@ -1,23 +1,19 @@
 import {userStore} from "../store/userStore.ts";
 import {useCallback} from "react";
-import RegisterError from "../Error/RegisterError.ts"
 
 export enum AuthStatus {
     Unknown = 0,
-    Authenticated = 1,
-    Guest = 2,
+    Authenticated = 1
 }
 
 const apiUrl: string = import.meta.env.VITE_FETCH_API;
 
 export function useAuth() {
     const {account, setAccount} = userStore();
+
     let status = AuthStatus.Unknown;
     switch (account) {
         case null :
-            status = AuthStatus.Guest;
-            break;
-        case undefined :
             status = AuthStatus.Unknown;
             break;
         default :
@@ -26,42 +22,14 @@ export function useAuth() {
     }
 
 
-    const tokenAuthenticate = useCallback(() => {
-        fetch(apiUrl + '/token-login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${localStorage.getItem('refreshToken') || ''}`
-            }
-        })
-            .then(res => {
-                if (!res.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return res.json();
-            })
-            .then((data) => {
-                setAccount(data);
-                localStorage.setItem('jwt', data.jwt);
-                localStorage.setItem('refreshToken', data.refreshToken);
-            })
-            .catch(()=> {
-                setAccount(null);
-                localStorage.removeItem('jwt');
-                localStorage.removeItem('refreshToken');
-            });
-    }, [])
-
-
-    const login = useCallback((email: string, password:string) => {
-        return fetch(apiUrl + '/login', {
+    const login = useCallback((codeId: number) => {
+        return fetch('http://localhost:3000/get-code-validity', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                email: email,
-                password: password
+                code: codeId
             })
         })
         .then(res => {
@@ -70,66 +38,17 @@ export function useAuth() {
             }
             return res.json();
         })
-        .then((data) => {
-            setAccount(data);
-            localStorage.setItem('jwt', data.jwt);
-            localStorage.setItem('refreshToken', data.refreshToken);
+        .then(() => {
+            setAccount(true);
             return true
         })
         .catch(()=>{return false});
     }, [])
 
 
-    const logout = useCallback(() => {
-        fetch(apiUrl + '/logout', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${account?.refreshToken || ''}`
-            }
-        })
-        .then(()=> {
-            setAccount(null);
-            localStorage.removeItem('jwt');
-            localStorage.removeItem('refreshToken');
-        })
-    }, [])
-
-    const register = useCallback((email: string, username: string, password:string) => {
-        return fetch(apiUrl + '/register', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                email,
-                password,
-                username
-            })
-        })
-        .then(res => {
-            if (!res.ok) {
-                return Promise.reject({...res.json(), status:res.status});
-            }
-            return res.json();
-        })
-        .then((data) => {
-            setAccount(data);
-            localStorage.setItem('jwt', data.jwt);
-            localStorage.setItem('refreshToken', data.refreshToken);
-            return true
-        })
-        .catch((error)=>{
-            return error
-        });
-    }, [])
-
     return {
         account,
         status,
-        tokenAuthenticate,
         login,
-        logout,
-        register
     }
 }
